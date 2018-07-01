7d4b8f4d1aae4ef7ef154957ddf34b32c5cafe45
Torbjörn Lönnemark <tobbez@ryara.net>
2018-07-01 15:46:19 +0200

    Update for recent versions of Chrome
    The old approach used to work on Chrome 48.0.2547.0-1.
    
    It does not work on the current version of Chrome on the dev channel
    (69.0.3472.3), which always passes exitType=4 and exitCode=0 to the
    onExited event.
    
    Fixes #1.

--- background.js
+++ background.js
@@ -1,33 +1,14 @@
-var savedTabs = {};
-function recordTabs () {
-        savedTabs = {};
-        chrome.tabs.query({currentWindow: true}, function(tabs) {
-                for (var i = 0; i < tabs.length; ++i) {
-                        (function (tabidx) {
-                        chrome.processes.getProcessIdForTab(tabs[tabidx].id, function (pid) {
-                                if (pid in savedTabs) {
-                                        savedTabs[pid].add(tabs[tabidx].id);
-                                } else {
-                                        savedTabs[pid] = new Set([tabs[tabidx].id]);
-                                }
-                        });
-                        })(i);
-                }
-        });
-}
-chrome.tabs.onUpdated.addListener(recordTabs);
-chrome.runtime.onStartup.addListener(recordTabs);
-chrome.runtime.onInstalled.addListener (recordTabs);
-
 // Apparently required for onExited to fire at all
 chrome.processes.onUpdated.addListener(function(process){});
 
 chrome.processes.onExited.addListener (function (id, exitType, exitCode) {
-        console.log(JSON.stringify([id, exitType, exitCode]));
-        console.log(savedTabs);
-        // 4 == normal, probably
-        if (exitType == 4 || !(id in savedTabs)) return;
-        savedTabs[id].forEach(function (v) {
-                chrome.tabs.reload(v);
-        });
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach(t => {
+      chrome.processes.getProcessIdForTab(t.id, pid => {
+        if (id === pid) {
+          chrome.tabs.reload(t.id);
+        }
+      });
+    });
+  });
 });
